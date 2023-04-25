d71b9fa97b3feed2b18d1eddfa1a60481d134162
Tybinka Krzysztof <krzysztof.tybinka@microsoft.wsei.edu.pl>
2023-04-25 16:05:55 +0200

    commit

---
+++ Infrastructure/EF/Mappers/QuizMappers.cs
@@ -0,0 +1,28 @@
+﻿using ApplicationCore.Models;
+using Infrastructure.EF.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.EF.Mappers
+{
+    public static class QuizMappers
+    {
+        public static QuizItem FromEntityToQuizItem(QuizItemEntity entity)
+        {
+            return new QuizItem(
+                entity.Id,
+                entity.Question,
+                entity.IncorrectAnswers.Select(e => e.Answer).ToList(),
+                entity.CorrectAnswer);
+        }
+
+        public static Quiz FromEntityToQuiz(QuizEntity entity)
+        {
+            var quizItems = entity.Items.Select(x => new QuizItem(x.Id, x.Question, x.IncorrectAnswers.Select(y => y.Answer).ToList(), x.CorrectAnswer)).ToList();
+            return new Quiz(entity.Id, quizItems, entity.Title);
+        }
+    }
+}
---
+++ Infrastructure/EF/Services/QuizUserServiceEF.cs
@@ -0,0 +1,113 @@
+﻿using ApplicationCore.Exceptions;
+using ApplicationCore.Interfaces;
+
+using ApplicationCore.Models;
+using Infrastructure.EF.Entities;
+using Infrastructure.EF.Mappers;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.EF.EF.Services
+{
+    public class QuizUserServiceEF : IQuizUserService
+    {
+        private readonly QuizDbContext _context;
+
+        public QuizUserServiceEF(QuizDbContext context) //10
+        {
+            this._context = context;
+        }
+
+        public Quiz CreateAndGetQuizRandom(int count)
+        {
+            Random random = new Random();
+            var quizzes = _context.Quizzes.ToList();
+            var quiz = quizzes[random.Next(quizzes.Count)];
+            return QuizMappers.FromEntityToQuiz(quiz);
+        }
+
+        public IEnumerable<Quiz> FindAllQuizzes()
+        {
+            return _context
+                .Quizzes
+                .AsNoTracking()
+                .Include(q => q.Items)
+                .ThenInclude(i => i.IncorrectAnswers)
+                .Select(QuizMappers.FromEntityToQuiz)
+                .ToList();
+        }
+
+        public Quiz? FindQuizById(int id)
+        {
+            var entity = _context
+                .Quizzes
+                .AsNoTracking()
+                .Include(q => q.Items)
+                .ThenInclude(i => i.IncorrectAnswers)
+                .FirstOrDefault(e => e.Id == id);
+            return entity is null ? null : QuizMappers.FromEntityToQuiz(entity);
+        }
+
+        public List<QuizItemUserAnswer> GetUserAnswersForQuiz(int quizId, int userId)
+        {
+            var quiz = _context.Quizzes.FirstOrDefault(x => x.Id == quizId);
+            if(quiz is null)
+            {
+                throw new QuizNotFoundException($"Quiz with id {quizId} not found");
+            }
+
+            var user = _context.Users.FirstOrDefault(x => x.Id == userId);
+            if(user is null)
+            {
+                throw new Exception("Uset not found");
+            }
+
+            return _context
+                .UsersAnswers
+                .Where(x => x.UserId == userId && x.QuizId == quizId)
+                .Include(x => x.QuizItem)
+                .Select(x => new QuizItemUserAnswer(QuizMappers.FromEntityToQuizItem(x.QuizItem), x.UserId, x.QuizId, x.UserAnswer))
+                .ToList();
+        }
+
+        public QuizItemUserAnswer SaveUserAnswerForQuiz(int quizId, int quizItemId, int userId, string answer)
+        {
+            QuizItemUserAnswerEntity entity = new QuizItemUserAnswerEntity()
+            {
+                UserId = userId,
+                QuizItemId = quizItemId,
+                QuizId = quizId,
+                UserAnswer = answer
+            };
+            try
+            {
+                var saved = _context.UsersAnswers.Add(entity).Entity;
+                _context.SaveChanges();
+                //_context.QuizItems.Attach TODO????
+                return new QuizItemUserAnswer()
+                {
+                    UserId = saved.UserId,
+                    QuizItem = QuizMappers.FromEntityToQuizItem(saved.QuizItem),
+                    QuizId = saved.QuizId,
+                    Answer = saved.UserAnswer
+                };
+            }
+            catch (DbUpdateException e)
+            {
+                if (e.InnerException.Message.StartsWith("The INSERT"))
+                {
+                    throw new QuizNotFoundException("Quiz, quiz item or user not found. Can't save!");
+                }
+                if (e.InnerException.Message.StartsWith("Violation of"))
+                {
+                    throw new QuizAnswerItemAlreadyExistsException(quizId, quizItemId, userId);
+                }
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
